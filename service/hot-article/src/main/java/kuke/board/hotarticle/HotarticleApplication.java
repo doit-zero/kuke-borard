39495e574024133d94f9b9acc6221f6690a3f01a
DoubleJ <131241458+doit-zero@users.noreply.github.com>
2025-01-08 23:32:18 +0900

    Initial commit

---
+++ service/hot-article/src/main/java/kuke/board/hotarticle/HotarticleApplication.java
@@ -0,0 +1,11 @@
+package kuke.board.hotarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotarticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotarticleApplication.class, args);
+    }
+}
