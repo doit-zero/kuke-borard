2445965c5ff2c0ba9b1a0b841493350cb6ac1343
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-01-30 15:41:05 +0900

    댓글 작성 api 테스트

---
+++ service/comment/src/test/java/kuke/board/comment/api/CommentApiTest.java
@@ -0,0 +1,42 @@
+package kuke.board.comment.api;
+
+import kuke.board.comment.service.response.CommentResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+
+public class CommentApiTest {
+
+    RestClient restClient = RestClient.create("http://localhost:9001");
+
+    @Test
+    void create(){
+        CommentResponse response = createComment(new CommentCreateRequest(1L, "comment-1", null, 1L));
+        CommentResponse response2 = createComment(new CommentCreateRequest(1L, "comment-2", response.getCommentId(), 1L));
+        CommentResponse response3 = createComment(new CommentCreateRequest(1L, "comment-3", response.getCommentId(), 1L));
+
+        System.out.println("commentId=%s".formatted(response.getCommentId()));
+        System.out.println("\tcommentId=%s".formatted(response2.getParentCommentId()));
+        System.out.println("\tcommentId=%s".formatted(response3.getParentCommentId()));
+    }
+
+
+    CommentResponse createComment(CommentCreateRequest request){
+        return restClient.post()
+                .uri("/v1/comments")
+                .body(request)
+                .retrieve()
+                .body(CommentResponse.class);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public class CommentCreateRequest {
+        private Long articleId;
+        private String content;
+        private Long parentCommentId;
+        private Long writerId;
+    }
+}
